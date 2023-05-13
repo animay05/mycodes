f60872c8ed75847d848eea190ec5c46f2581797c
Animay Awasthi <animayawasthi05@gmail.com>
2023-05-14 03:54:37 +0530

    Add files via upload

---
+++ codes/21.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+ 
+class Base {
+    int i;
+    public:
+        Base(int i = 0): i(i) { }
+        virtual int val() const { return i; }
+        virtual ~Base() { }
+};
+ 
+class Derived : public Base
+{
+    int i;
+    public:
+        Derived(int i = 0): i(i) { }
+        virtual int val() const { return i; }
+        virtual ~Derived() { }
+};
+ 
+class MostDerived : public Derived 
+{
+    int i;
+    public:
+        MostDerived(int i = 0): i(i) { }
+        virtual int val() const { return i; }
+        virtual ~MostDerived() { }
+};
+ 
+int main()
+{
+    Base * B = new Base(1);
+    Base * D = new Derived(2);
+    Base * MD = new MostDerived(3);
+ 
+    std::cout << "Base.Value() = " << B->val() << std::endl;
+    std::cout << "Derived.Value() = " << D->val() << std::endl;
+    std::cout << "MostDerived.Value() = " << MD->val() << std::endl;
+}
